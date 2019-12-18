919a627e22d8b3068c2b01421cd1fc0ce0d20bd7
Brattlof <57799495+Brattlof@users.noreply.github.com>
2019-12-18 14:25:24 +0100

    Initial commit

---
+++ UnityHideMethod/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Base : MonoBehaviour
+{
+
+    public static GameObject handle;
+
+    public static void Init()
+    {
+    }
+
+    public void OnGui()
+    {
+    }
+
+    public void Update()
+    {
+    }
+
+}
+
+public class Derived : Base
+{
+
+    public static void Init()
+    {
+        handle = new GameObject();
+        handle.AddComponent<Derived>();
+        DontDestroyOnLoad(handle);
+    }
+
+    public void OnGui()
+    {
+        //Functions
+    }
+
+    public void Update()
+    {
+        //Functions
+    }
+
+}
+
+class Final
+{
+
+  
+    static public void Main()
+    {
+
+
+        Derived obj = new Derived();
+
+
+        obj.OnGui();
+        obj.Update();
+
+    }
+}
